b10aa604bf8afebedbdae0d40b17dc3a25818092
Alexander Staubo <alex@bengler.no>
2016-05-20 14:28:01 -0400

    Update to latest go-pebbleclient.

--- client.go
+++ client.go
@@ -1,32 +1,19 @@
 package hydrantclient
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
-	"net/http"
 
 	"github.com/pkg/errors"
-	"github.com/t11e/go-pebbleclient"
+	pc "github.com/t11e/go-pebbleclient"
 )
 
 type Client struct {
-	client *pebbleclient.Client
+	c pc.Client
 }
 
 // New constructs a new client.
-func New(client *pebbleclient.Client) (*Client, error) {
-	return &Client{client}, nil
-}
-
-// NewFromRequest constructs a new client from an HTTP request.
-func NewFromRequest(options pebbleclient.ClientOptions, req *http.Request) (*Client, error) {
-	if options.AppName == "" {
-		options.AppName = "hydrant"
-	}
-	client, err := pebbleclient.NewFromRequest(options, req)
-	if err != nil {
-		return nil, err
-	}
+func New(client pc.Client) (*Client, error) {
 	return &Client{client}, nil
 }
 
@@ -37,10 +24,11 @@ func (client *Client) Query(query *Query, dataset string) (*ResultSet, error) {
 	}
 
 	result := ResultSet{}
-	err = client.client.Post(fmt.Sprintf("/query/%s/json", dataset), pebbleclient.Body{
-		Data:        body,
-		ContentType: "application/json",
-	}, &result)
+	err = client.c.Post("/query/:dataset/json", &pc.RequestOptions{
+		Params: pc.Params{
+			"dataset": dataset,
+		},
+	}, bytes.NewReader(body), &result)
 	if err != nil {
 		return nil, err
 	}
